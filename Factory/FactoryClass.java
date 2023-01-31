d606bafa31853811cf1cca86886fec812ed64495
Mondin0 <gabmondino@gmail.com>
2023-01-31 17:25:23 -0300

    Agregando comentarios

--- Factory/FactoryClass.java
+++ Factory/FactoryClass.java
@@ -4,8 +4,18 @@ package Factory;
 * */
 public class FactoryClass {
     public static void main(String[] args) {
+
+        //Llamamos a la clase factory que dependiendo el tipo que le pasemos nos retornara un objeto de esa misma clase
+
         Figura figura1 = CreadorDeFigura.crearFigura(Tipos.TRIANGULO);
-        Triangulo triangulo = (Triangulo) figura1;
+        Triangulo triangulo = (Triangulo) figura1; //Forzamos el cambio de tipo, ya que cradorDeFigura nos retorna un objeto del tipo Figura, que es abstracta
+        Figura figura2= CreadorDeFigura.crearFigura(Tipos.CIRCULO);
+        Circulo circulo = (Circulo) figura2;
+        Figura figura3 = CreadorDeFigura.crearFigura(Tipos.RECTANGULO);
+        Rectangulo rectangulo = (Rectangulo) figura3;
+
+        //mostramos en pantalla lo que obtengamos de cada objeto
+
         triangulo.setAltura(5);
         triangulo.setBase(3);
         System.out.println("base= " + triangulo.getBase());
@@ -14,16 +24,14 @@ public class FactoryClass {
 
         System.out.println("-------------------------------");
 
-        Figura figura2= CreadorDeFigura.crearFigura(Tipos.CIRCULO);
-        Circulo circulo = (Circulo) figura2;
+
         circulo.setRadio(5);
         System.out.println("radio = " + circulo.getRadio());
         System.out.println("Area del circulo = " + Math.round(circulo.calcularArea()));
 
         System.out.println("-------------------------------");
 
-        Figura figura3 = CreadorDeFigura.crearFigura(Tipos.RECTANGULO);
-        Rectangulo rectangulo = (Rectangulo) figura3;
+
         rectangulo.setAlto(2);
         rectangulo.setAncho(6);
         System.out.println("Alto = " + rectangulo.getAlto());
